242623d6e450d41e73f92044d0fd1afe3aabb8a8
Martins <aremartins801@gmail.com>
2026-01-26 11:41:24 +0000

    fix

--- src/plugins/core.js
+++ src/plugins/core.js
@@ -83,12 +83,8 @@ export default {
             } catch (e) {}
           }
           
-          // Force exit immediately and start the root index.js (recloner)
-          if (process.platform === 'win32') {
-            execSync('start /b cmd /c "timeout /t 1 >nul & node index.js"');
-          } else {
-            execSync('nohup node index.js &');
-          }
+          // Force exit immediately and start the root index.js (recloner) in the foreground (interactive)
+          execSync('node index.js', { stdio: 'inherit' });
           process.exit(0);
           return;
         }
